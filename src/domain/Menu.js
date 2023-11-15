bea6b2bb416f7378ac3a87860d5051db78fa4ffb
Myomyoring <narcissuscat@naver.com>
2023-11-15 23:19:04 +0900

    [refactor] 에러 부분 depth 수정 및 분리

--- src/domain/Menu.js
+++ src/domain/Menu.js
@@ -15,28 +15,43 @@ class Menu {
 		const menuNames = this.#separate(orderHistory);
 		this.#validate(menuNames);
 	}
+	#eachSeparate(orderMenu) {
+		const menu = orderMenu.split('-');
+		const menuName = menu[0];
+		const count = Number(menu[1]);
+		if (!Validation.menuCount(count)) {
+			throw new Error();
+		}
+		return { menuName, count };
+	}
 	#separate(orderHistory) {
 		let totalMenuCount = 0;
 		orderHistory.forEach((orderMenu) => {
-			const menu = orderMenu.split('-');
-			const menuName = menu[0];
-			const count = Number(menu[1]);
-			if (!Validation.menuCount(count)) throw new Error();
+			const { menuName, count } = this.#eachSeparate(orderMenu);
 			totalMenuCount += count;
 			this.#orderSheet(menuName, count);
 		});
-		if (!Validation.menuCount(totalMenuCount)) throw new Error();
+		if (!Validation.menuCount(totalMenuCount)) {
+			throw new Error();
+		}
 		return this.#userOrderSheet.map((menu) => menu.menuName);
+	}
+	#eachValidate(userMenu) {
+		if (!Validation.menuExist(userMenu)) {
+			throw new Error();
+		}
 	}
 	#validate(menuNames) {
 		if (!Validation.menuDuplicate(menuNames)) {
 			throw new Error();
 		}
 		menuNames.forEach((userMenu) => {
-			if (!Validation.menuExist(userMenu)) throw new Error();
+			this.#eachValidate(userMenu);
 			this.#orderMenuInfo.push(MENU.find((menu) => menu.name === userMenu));
 		});
-		if (!Validation.menuOnlyBeverage(this.#orderMenuInfo)) throw new Error();
+		if (!Validation.menuOnlyBeverage(this.#orderMenuInfo)) {
+			throw new Error();
+		}
 	}
 	#orderSheet(menuName, count) {
 		let order = {};
